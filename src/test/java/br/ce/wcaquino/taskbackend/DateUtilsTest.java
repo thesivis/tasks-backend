182aa8bf9bffbca99f1f980a06948c65f0b5f9a8
thesivis <raphael@ic.ufmt.br>
2024-05-22 19:45:13 -0400

    Adiciona um novo teste

--- src/test/java/br/ce/wcaquino/taskbackend/DateUtilsTest.java
+++ src/test/java/br/ce/wcaquino/taskbackend/DateUtilsTest.java
@@ -16,4 +16,11 @@ public class DateUtilsTest {
         boolean result = DateUtils.isEqualOrFutureDate(before);
         assertFalse(result);
     }
+
+    @Test
+    public void isAfter() {
+        LocalDate before = LocalDate.of(2100, 1, 1);
+        boolean result = DateUtils.isEqualOrFutureDate(before);
+        assertFalse(result);
+    }
 }
